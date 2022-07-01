889b835add570dab9ca9fd6b29ea44a45805c102
ParadoxoID <asker01adig@gmail.com>
2022-07-01 15:07:54 +0300

    Добавил компонент header и лого

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import HomePage from './pages/homepage/HomePage';
 import ShopPage from './pages/shop/ShopPage';
 
 import './App.css';
+import Header from './components/Header/Header';
 
 const App = () => {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
